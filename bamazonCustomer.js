b210b6bfcc63b3624bc2b8d9ac3c62419167f2f0
1Cbondy1 <claytonbondy@gmail.com>
2018-11-29 02:03:12 -0600

    added function to update stock_quantity in database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,7 +24,6 @@ function afterConnection() {
         }
         console.log("\n");
         whichItem();
-        connection.end();
     });
 }
 
@@ -39,7 +38,7 @@ function whichItem() {
         var userItem = parseInt(user.item);
         if ((Number.isInteger(userItem)) && (userItem < 11) &&  (userItem > -1)) {
             console.log("Awesome!");
-            howMany();
+            howMany(userItem);
         }
         else {
             console.log("Please enter a valid item ID.")
@@ -48,7 +47,7 @@ function whichItem() {
     });
 }
 
-function howMany() {
+function howMany(userItem) {
     inquirer.prompt([
         {
         type: "input",
@@ -56,13 +55,36 @@ function howMany() {
         message: "How many of the item you would like to purchase?"
         },
     ]).then(function(user) {
-        var userItem = parseInt(user.item);
-        if ((Number.isInteger(userItem)) && (userItem > 0)) {
+        var itemNumber = parseInt(user.item);
+        if ((Number.isInteger(itemNumber)) && (itemNumber > 0)) {
             console.log("Awesome!");
+            updateProduct(userItem, itemNumber);
         }
         else {
             console.log("Please enter a valid amount.")
             howMany();
         }
+    });
+}
+
+function updateProduct(userItem, itemNumber) {
+    console.log(userItem, itemNumber);
+    var query = connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - " + itemNumber + " WHERE id = " + userItem,
+        function(err, res) {
+            displayInfo();
+            connection.end();
+        }
+    );
+}
+
+function displayInfo() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log("\n");
+        for (var i = 0; i < 10; i++) {
+            console.log(res[i].id + " " + res[i].product_name + " " + res[i].department_name + " " +  "$" + res[i].price + " " + res[i].stock_quantity);
+        }
+        console.log("\n");
     });
 }
