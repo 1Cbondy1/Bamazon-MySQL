e5aa68604ffb24441485e23a0a339fd63b64044b
1Cbondy1 <claytonbondy@gmail.com>
2018-11-29 20:09:22 -0600

    added column.table package and ~flair~

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,7 +12,9 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    console.log("connected as id " + connection.threadId)
+    santa();
+    console.log("\nWELCOME TO FANCY SANTA'S CHRISTMAS SUPER STORE!\n");
     afterConnection();
 });
   
@@ -19,9 +22,18 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         console.log("\n");
-        for (var i = 0; i < 10; i++) {
-            console.log(res[i].id + " " + res[i].product_name + " " + res[i].department_name + " " +  "$" + res[i].price + " " + res[i].stock_quantity);
+        var productTable = [];
+        for (var i = 0; i < res.length; i++) {
+                var tempTable = {
+                    ID: res[i].id,
+                    Product: res[i].product_name,
+                    Department: res[i].department_name,
+                    Price: res[i].price,
+                    Quantity: res[i].stock_quantity
+                }
+                productTable.push(tempTable);
         }
+        console.log(console.table(productTable));
         console.log("\n");
         whichItem();
     });
@@ -32,16 +44,20 @@ function whichItem() {
         {
         type: "input",
         name: "item",
-        message: "What is the ID of the item you would like to purchase?"
+        message: "What is the ID of the item you would like to purchase? [Quit with Q]"
         },
     ]).then(function(user) {
         var userItem = parseInt(user.item);
         if ((Number.isInteger(userItem)) && (userItem < 11) &&  (userItem > -1)) {
-            console.log("Awesome!");
+            console.log("\nPurchasing item...\n");
             howMany(userItem);
         }
+        else if (user.item === "q" || user.item === "Q") {
+            console.log("\nEnding connection...\n")
+            connection.end();
+        }
         else {
-            console.log("Please enter a valid item ID.")
+            console.log("\nPlease enter a valid item ID.\n")
             whichItem();
         }
     });
@@ -57,34 +73,82 @@ function howMany(userItem) {
     ]).then(function(user) {
         var itemNumber = parseInt(user.item);
         if ((Number.isInteger(itemNumber)) && (itemNumber > 0)) {
-            console.log("Awesome!");
             updateProduct(userItem, itemNumber);
         }
         else {
-            console.log("Please enter a valid amount.")
+            console.log("\nPlease enter a valid amount.\n")
             howMany();
         }
     });
 }
 
 function updateProduct(userItem, itemNumber) {
-    console.log(userItem, itemNumber);
-    var query = connection.query(
-        "UPDATE products SET stock_quantity = stock_quantity - " + itemNumber + " WHERE id = " + userItem,
-        function(err, res) {
-            displayInfo();
-            connection.end();
-        }
-    );
-}
-
-function displayInfo() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log("\n");
-        for (var i = 0; i < 10; i++) {
-            console.log(res[i].id + " " + res[i].product_name + " " + res[i].department_name + " " +  "$" + res[i].price + " " + res[i].stock_quantity);
+        var supply = res[userItem-1].stock_quantity;
+        var price = res[userItem-1].price * itemNumber;
+        if (itemNumber < supply) {
+            var query = connection.query(
+                "UPDATE products SET stock_quantity = stock_quantity - " + itemNumber + " WHERE id = " + userItem,
+                function(err, res) {
+                    console.log("\nThank you for your purchase! Your total is: $" + price + ".");
+                    afterConnection();
+                }
+            );
+        } else {
+            console.log("\nInsufficient supply!\n");
+            howMany(userItem);
         }
-        console.log("\n");
     });
+}
+
+function santa() {
+console.log(
+`
+                                ,,,,,,,,,,,,,,,,.. 
+                        ..,,;;;;;;;;;;;;;;;;;;;;;;;;;;,,. 
+                    .,::::;;;;aaaaaaaaaaaaaaaaaaaaaaaaaaa;;,,. 
+                .,;;,:::a@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@a, 
+              ,;;;;.,a@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@a 
+           ,;;;;%;.,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@a, 
+        ,;%;;;;%%;,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+     ,;;%%;;;;;%%;;@@@@@@@@@@@@@@'%v%v%v%v%v%v%v%v%v%v%v%v @@@@@@@@@ 
+   ,;;%%;;;;:;;;%;;@@@@@@@@@'%vvvvvvvvvnnnnnnnnnnnnnnnnvvvvvv% @@@@' 
+  ,;%%;;;;;:;;;;;;;;@@@@@'%vvva@@@@@@@@avvnnnnnnnnnnvva@@@@@@@OOov, 
+ ,;%;;;;;;:::;;;;;;;@@'OO%vva@@@@@@@@@@@@vvnnnnnnnnvv@@@@@@@@@@@Oov 
+ ;%;;;;;;;:::;;;;;;;;'oO%vvn@@%nvvvvvvvv%nnnnnnnnnnnnn%vvvvvvnn%@Ov 
+ ;;;;;;;;;:::;;;;;;::;oO%vvnnnn>>nn.    nnnnnnnnnnnn>>nn.    nnnvv' 
+ ;;;;;;;;;:::;;;;;;::;oO%vvnnvvmmmmmmmmmmvvvnnnnnn;%mmmmmmmmmmmmvv, 
+ ;;;;;;;;;:::;;;;;;::;oO%vvmmmmmmmmmmmmmmmmmvvnnnv;%mmmmmmmmmmmmmmmv, 
+ ;;;;;;;;;;:;;;;;;::;;oO%vmmmmnnnnnnnnnnnnmmvvnnnvmm;%vvnnnnnnnnnmmmv 
+   ;%;;;;;;;:;;;;::;;o@@%vvmmnnnnnnnnnnnvnnnnnnnnnnmmm;%vvvnnnnnnmmmv 
+    ;;%%;;;;;:;;;::;.oO@@%vmmnnnnnnnnnvv%;nnnnnnnnnmmm;%vvvnnnnnnmmv' 
+      ;;;%%;;;:;;;::;.o@@%vvnnnnnnnnnnnvv%;nnnnnnnmm;%vvvnnnnnnnv%'@a. 
+      a ;;;%%;;:;;;::;.o@@%vvvvvvvvvvvvvaa@@@@@@@@@@@@aa%%vvvvv%%@@@@o. 
+     .@@o ;;;%;;;;;;::;,o@@@%vvvvvvva@@@@@@@@@@@@@@@@@@@@@avvvva@@@@@%O, 
+    .@@@@@Oo ;;;;;;;;::;o@@@@@@@@@@@@@@@@@@@@"""""""@@@@@@@@@@@@@@@@@OO@a 
+  .@@@@@@@@@OOo ;;;;;;:;o@@@@@@@@@@@@@@@@"           "@@@@@@@@@@@@@@oOO@@@, 
+ .@@@@o@@@@@@@OOo ;;;;:;o,@@@@@@@@@@%vvvvvvvvvvvvvvvvvv%%@@@@@@@@@oOOO@@@@@, 
+ @@@@o@@@@@@@@@OOo;::;'oOOooooooooOOOo%vvvvvvvvvvvvvv%oOOooooooooOOO@@@O@@@, 
+ @@@oO@@@@@@@@@OOa@@@@@a,oOOOOOOOOOOOOOOoooooooooooooOOOOOOOOOOOOOO@@@@Oo@@@ 
+ @@@oO@@@@@@@OOa@@@@@@@@Oo,oO@@@@@@@@@@OOOOOOOOOOOOOO@@@@@@@@@@@@@@@@@@Oo@@@ 
+ @@@oO@@@@@@OO@@@@@@@@@@@OO,oO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Oo@@@ 
+ @@@@o@@@@@@OO@@@@@@@@@@OOO,oO@@@@@@@@@O@@@@@@@@@@@@@@@@@@@@@o@@@@@@@@@O@@@@ 
+ @@@@@o@@@@@OOo@@@@@@@OOOO'oOO@@@@@@@@Oo@@@@@@@@@@@@O@@@@@@@@Oo@@@@@@@@@@@@a 
+ @@@@@@@O@@@OOOo OOOOOOO'oOO@@@@@@@@@O@@@@@@@@@@@@@@@O@@@@@@@@Oo@@@@@@@@@@@@ 
+  @@@@@OO@@@@@OOOooooooooOO@@@@@@@@@@@@@@@@@@@@@@@@@@Oo@@@@@@@Oo@@@@@@oO@@@@ 
+    @@@OO@@@@@@@@@@@@@@@@@@@O@@@@@@@@@@@@@@@@@@@@@@@@Oo@@@@@@@O@@@@@@@oO@@@' 
+       @@ O@@@@@@@@@@@@@@@@@@@Oo@@@@@@@@@@@@@@@@@@@@@@Oo@@@@@@@@@@@@@@@O@@@' 
+         @ @@@@@@@@@@@@@@@@@@@OOo@@@@@@@@@@@@@@@@@@@@@O@@@@@@@@@@@@@@@'@@' 
+            @@@@@@@@@@@@@@@@@@OOo@@@@@@@@@@@@@@@@@@@@O@@@@@@@@@@@@@@@ a' 
+                @@@@@@@@@@@@@@OOo@@@@@@@@@@@@@@@@@@@@@@@@Oo@@@@@@@@' 
+                   @@@@@@@@@@@Oo@@@@@@@@@@@@@@@@@@@@@@@@@Oo@@@@' 
+                       @@@@@@Oo@@@@O@@@@@@@@@@@@@@@@@@@'o@@' 
+                           @@@@@@@@oO@@@@@@@@@@@@@@@@@ a' 
+                               @@@@@oO@@@@@@@@@@@@@@' ' 
+                                   @@@o' @@@@@@@@' 
+                                      @' .@@@@' 
+                                         @@' 
+`
+    );
 }
